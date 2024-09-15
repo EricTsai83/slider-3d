df1c43ff35596ba16d7969a296d5f0c5f184ef91
Eric Tsai <eric492718@gmail.com>
2024-09-15 14:03:49 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,55 @@
 import Image from "next/image";
+import styles from "@/styles/3d-silder.module.css";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const images = [
+    { src: "/images/dragon_1.jpg", alt: "Dragon 1", position: 1 },
+    { src: "/images/dragon_2.jpg", alt: "Dragon 2", position: 2 },
+    { src: "/images/dragon_3.jpg", alt: "Dragon 3", position: 3 },
+    { src: "/images/dragon_4.jpg", alt: "Dragon 4", position: 4 },
+    { src: "/images/dragon_5.jpg", alt: "Dragon 5", position: 5 },
+    { src: "/images/dragon_6.jpg", alt: "Dragon 6", position: 6 },
+    { src: "/images/dragon_7.jpg", alt: "Dragon 7", position: 7 },
+    { src: "/images/dragon_8.jpg", alt: "Dragon 8", position: 8 },
+    { src: "/images/dragon_9.jpg", alt: "Dragon 9", position: 9 },
+    { src: "/images/dragon_10.jpg", alt: "Dragon 10", position: 10 },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <div className="w-full h-screen text-center overflow-hidden relative">
+      <div
+        className="absolute w-[100px] h-[150px] top-[10%] left-[calc(50%-50px)] z-20 transform-style-3d animate-autoRun sm:w-[200px] sm:h-[250px] sm:left-[calc(50%-100px)]"
+        style={{ "--quantity": images.length } as React.CSSProperties}
+      >
+        {images.map((image) => (
+          <div
+            key={image.position}
+            className="absolute inset-0 custom-transform-z-180 sm:custom-transform-z-550"
+            style={{ "--position": image.position } as React.CSSProperties}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <Image src={image.src} alt={image.alt} width={200} height={250} />
+          </div>
+        ))}
+      </div>
+
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full h-max pb-28 flex justify-center items-center">
+        <div className="flex flex-col gap-4 items-center">
+          <h1
+            data-content="CSS ONLY"
+            className="relative text-6xl sm:text-9xl text-[#25283b] font-bold text-shadow-lg shadow-[#25283b]/50 after:text-outline after:text-transparent after:z-20 after:content-[attr(data-content)] after:absolute after:inset-0"
           >
-            Read our docs
-          </a>
+            CSS ONLY
+          </h1>
+          <div className="text-white p-5 text-shadow-sm z-20 max-w-none text-center py-0 px-7">
+            <h2 className="text-5xl">Eric Tsai</h2>
+            <p>
+              <b>Web Design</b>
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="bg-[url('/images/model.png')] absolute bottom-0 left-0 w-full h-[75vh] bg-auto bg-top bg-no-repeat z-10"></div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -8,12 +9,65 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      textShadow: {
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
+      },
+      keyframes: {
+        autoRun: {
+          "0%": {
+            transform: "perspective(1000px) rotateX(-16deg) rotateY(0deg)",
+          },
+          "100%": {
+            transform: "perspective(1000px) rotateX(-16deg) rotateY(360deg)",
+          },
+        },
+      },
+      animation: {
+        autoRun: "autoRun 20s linear infinite",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        ".text-outline": {
+          "-webkit-text-stroke": "2px #d2d2d2",
+        },
+        ".text-outline-hover:hover": {
+          "-webkit-text-stroke": "2px #d2d2d2",
+        },
+      };
+
+      addUtilities(newUtilities);
+    }),
+
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") },
+      );
+    }),
+
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".custom-transform-z-550": {
+          transform:
+            "rotateY(calc((var(--position) - 1) * (360 / var(--quantity)) * 1deg)) translateZ(550px)",
+        },
+        ".custom-transform-z-180": {
+          transform:
+            "rotateY(calc((var(--position) - 1) * (360 / var(--quantity)) * 1deg)) translateZ(180px)",
+        },
+        // Add more variations as needed
+      });
+    }),
+    require("tailwindcss-3d"),
+  ],
 };
 export default config;
